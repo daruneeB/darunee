9cfde12495eab6e17b4d0589095d4368674fdbf3
rachatach <thaimyhomebiz@gmail.com>
2022-06-29 19:12:10 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include "navbar.php";
+
+/* echo "<a href='index.php'>ติดต่อเรา </a>" ;
+echo "<a href='contact.php'>ติดต่อเรา </a>" ;
+echo "<a href='about.php'>เกี่ยวกับเรา </a>" ;
+ */
+
+
+echo" <h1> ยินดีต้อนรับเข้าสู่เว็บไซต์ </h1> ";
+
+
+date_default_timezone_set('asia/bangkok');
+echo"วันนี้" .date('D M Y');
+echo"เวลา" .date('H:i:s')
+
+
+?>
---
+++ navbar.php
@@ -0,0 +1,3 @@
+<a href="index.php"> หน้าหลัก </a>
+<a href="contact.php"> ติดต่อเรา </a>
+<a href="about.php"> เกี่ยวกับ </a>
